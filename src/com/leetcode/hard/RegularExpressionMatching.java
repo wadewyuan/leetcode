a58e9f602c8234211f358731d83a361239820706
wadewyuan <wadeyuanwy@hotmail.com>
2016-11-01 22:54:57 +0800

    Problem #10 with partial solution

--- src/com/leetcode/hard/RegularExpressionMatching.java
+++ src/com/leetcode/hard/RegularExpressionMatching.java
@@ -24,6 +24,47 @@ package com.leetcode.hard;
  */
 public class RegularExpressionMatching {
     public boolean isMatch(String s, String p) {
-        return false;
+        int j = 0;
+        boolean asteriskMatch = false;
+        for(int i = 0; i < p.length(); i++) {
+            if(i >= p.length() - 1 && j < s.length() - 1) {
+                return false; // pattern is complete, but string still remains
+            }
+            if(p.charAt(i) != '*') {
+                if((i + 1 < p.length()) && p.charAt(i + 1) == '*') {
+                    asteriskMatch = true;
+                    for(; j < s.length(); ) {
+                        if(!isCharMatch(s.charAt(j), p.charAt(i))) {
+                            asteriskMatch = false;
+                            break;
+                        } else {
+                            j++;
+                        }
+                    }
+                } else {
+                    if(j >= s.length() - 1 && i <= p.length() - 1) {
+                        return false;
+                    }
+                    if(!isCharMatch(s.charAt(j++), p.charAt(i)) && !asteriskMatch) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     *
+     * @param c1 input character
+     * @param c2 character to match
+     * @return true if c1 equals to c2 or c2 is '.'
+     */
+    private boolean isCharMatch(char c1, char c2) {
+        return c1 == c2 || c2 == '.';
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new RegularExpressionMatching().isMatch("aaabcc", ".*c")); // failed with current code
     }
 }
