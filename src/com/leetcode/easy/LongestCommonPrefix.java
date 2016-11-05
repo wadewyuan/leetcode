4835a91d6972906042c2e46d1af03a882baadd71
wadewyuan <wadeyuanwy@hotmail.com>
2016-11-06 00:17:15 +0800

    Problem #14 - add to repo

---
+++ src/com/leetcode/easy/LongestCommonPrefix.java
@@ -0,0 +1,13 @@
+package com.leetcode.easy;
+
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ *
+ * Created by wadey on 11/6/2016.
+ */
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = "";
+        return prefix;
+    }
+}
