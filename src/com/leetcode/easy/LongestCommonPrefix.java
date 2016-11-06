1f30505e09bd9f2039da1b1405b75a8bfa176206
wadewyuan <wadeyuanwy@hotmail.com>
2016-11-06 10:14:08 +0800

    Problem #14 solved
    Regular scanning and binary search

--- src/com/leetcode/easy/LongestCommonPrefix.java
+++ src/com/leetcode/easy/LongestCommonPrefix.java
@@ -7,7 +7,57 @@ package com.leetcode.easy;
  */
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
-        String prefix = "";
+        if(strs == null || strs.length < 1) {
+            return "";
+        }
+        String prefix = strs[0];
+        for(int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if(prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
         return prefix;
+    }
+
+    public String longestCommonPrefixBinarySearch(String[] strs) {
+        if(strs == null || strs.length < 1) {
+            return "";
+        }
+        int minLen = Integer.MAX_VALUE;
+        for(String str : strs) {
+            if (str.length() < minLen) minLen = str.length();
+        }
+
+        int l = 0, r = minLen;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if(isCommonPrefix(strs, mid)) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return strs[0].substring(0, (l + r) / 2);
+    }
+
+    private boolean isCommonPrefix(String[] strs, int len){
+        String str1 = strs[0].substring(0,len);
+        for (int i = 1; i < strs.length; i++)
+            if (!strs[i].startsWith(str1))
+                return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(new LongestCommonPrefix().getCommonPrefix("abccddd", "abccdddc"));
+        System.out.println(new LongestCommonPrefix().longestCommonPrefixBinarySearch(new String[] {
+                "abccddd",
+                "ab",
+                "abcadddc",
+                "abcedddc"
+        }));
     }
 }
